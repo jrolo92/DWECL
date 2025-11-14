e1a1195a71e749711141bc3502f2088e7fc1148c
Javier RL <colego@hotmail.es>
2025-11-14 13:14:11 +0100

    Clase 14/11/25

--- Tema 6 - Eventos y formularios/ejercicios/ejercicio1/ejercicio1.js
+++ Tema 6 - Eventos y formularios/ejercicios/ejercicio1/ejercicio1.js
@@ -3,13 +3,16 @@ class BajaVehiculo {
         this.#fEvento();    // Cuando se cree un objeto de esta clase se va a disparar el fEvento directamente y estará preparado para los posibles eventos futuros
     }
 
+    // Método que va a estar preparado para los posibles eventos que se produzcan en el formulario.
     #fEvento() {
         // Obtenemos los datos con los que se van a disparar eventos por su id.
         var dni = document.getElementById("dni");
         var matricula = document.getElementById("matricula");
 
         // Controladores de los futuros eventos.
-        dni.addEventListener('click', () => this.#cambiarEtiqueta());           // Cuando se haga click en DNI se ejecuta el método cambiarEtiqueta
+        // Con la funcion flecha te aseguras de que el this hace referncia a la clase y no al input. Con el bind tambien nos aseguramos de que sea de la clase
+        dni.addEventListener('focus', () => this.#cambiarEtiqueta());           // Cuando se haga click en DNI se ejecuta el método cambiarEtiqueta
+        // dni.addEventListener('focus', this.#cambiarEtiqueta.bind(this));
         dni.addEventListener('blur', () => this.#etiquetaOriginal());           // Cuando se salga del DNI se ejecuta el método etiquetaOriginal
         matricula.addEventListener('keyup', () => this.#validarMatricula());
     }
@@ -27,12 +30,13 @@ class BajaVehiculo {
     #validarMatricula() {
         var matricula = document.getElementById("matricula");
         // El método checkValidity devolverá true si el campo cumple todas las restricciones html (required, pattern o type)
-        if (matricula.checkValidity() == false) {                      // Cuando sea false
+        if (matricula.validity.patternMismatch) {                      // Cuando sea false
             matricula.setCustomValidity("Tiene que ser: 1234ABC");     // Devolverá el mensaje personalizado
         } else {
             matricula.setCustomValidity("");
         }
     }
+
 }
 
 // La instanciamos con un onload si vamos a poner el codigo interno en el html
---
+++ Tema 6 - Eventos y formularios/ejercicios/ejercicio2/ejercicio2.js
@@ -0,0 +1,63 @@
+class Sesion {
+    // El atributo será un array de objetos.
+    #usuarios = [
+        {user: "pepe", password: "Aa123456"},
+        {user: "peri", password: "Bb123456"},
+        {user: "paco", password: "Cc123456"}
+    ]
+
+    /* 
+        Cada vez que se cree una instancia de la clase Sesion se va a disparar el fEvento
+        que estará preparado para los posibles eventos
+    */
+    constructor(){
+        this.#fEvento(); 
+    }
+
+    /*
+        Método que va a estar preparado para disparar otros métodos cuando se cumplan ciertos eventos
+    */
+    #fEvento(){
+        // Al escribir la contraseña se va a validar con el método 
+        var password = document.getElementById("password");
+        password.addEventListener('keyup', ()=> this.#validarPassword());
+        // Al iniciar sesión se va a validar con el método validarSesion()
+        var sesion = document.getElementById("sesion");
+        sesion.addEventListener('submit', ()=> this.#validarSesion());
+    }
+
+
+    /*
+        Método que comprueba si la contraseña introducida en el formulario coincide con el patrón especificado.
+    */
+    #validarPassword(){
+        var password = document.getElementById("password");
+        if (password.validity.patternMismatch){
+            password.setCustomValidity("El formato debe ser: Aa123456");
+        } else {
+            password.setCustomValidity("");
+        }
+    }
+
+    /*
+        Método que comprueba si el usuario y contraseña coinciden con los almacenados en el atributo usuarios.
+    */
+    #validarSesion(){
+        // Recogemos los valores de usuario y contraseña del formulario.
+        var userForm = document.getElementById("user").value;
+        var passForm = document.getElementById("password").value;
+        // De primeras el mensaje será de no coincidencia
+        var mensaje = "No coinciden usuario o contraseña";
+        // Recorremos el array de usuarios y si coinciden cambiamos el valor del mensaje
+        for (var usuario of this.#usuarios){
+            if (usuario.user === userForm && usuario.password === passForm ){
+                var mensaje = "Se ha iniciado sesión";
+                break;
+            }
+        }
+        // Muestra el mensaje
+        alert(mensaje);
+    }
+}
+
+new Sesion();
