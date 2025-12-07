65c848f1689eb7f8ccf918223248b3dba3f5ec1c
Javier RL <jrodlop271@g.educaand.es>
2025-12-07 15:12:18 +0100

    Examen 2

--- examen2/examen2_1eva_25_26_JavierRdguez.js
+++ examen2/examen2_1eva_25_26_JavierRdguez.js
@@ -1,21 +1,24 @@
 class Secc {
 
-    constructor(){}
-
-    cabecera(){
+    constructor() { }
+    /*
+        Método que crea una fila para la cabecera y le añade celdas con el nombre de cada columna y estilos.
+        Retorna: fila.
+    */
+    cabecera() {
         var fila = document.createElement("tr");
 
         var celdaCod = document.createElement("th");
         celdaCod.textContent = "ID";
-        celdaCod.className= "centra borde";
+        celdaCod.className = "centra borde";
 
         var celdaDes = document.createElement("th");
         celdaDes.textContent = "NOMBRE";
-        celdaDes.className= "centra borde";
+        celdaDes.className = "centra borde";
 
         var celdaCos = document.createElement("th");
         celdaCos.textContent = "COSTE €";
-        celdaCos.className= "centra borde";
+        celdaCos.className = "centra borde";
 
         fila.appendChild(celdaCod);
         fila.appendChild(celdaDes);
@@ -24,7 +27,11 @@ class Secc {
         return fila;
     }
 
-    modeloCartera(cod, descrip){
+    /*
+        Método que crea una fila y le añade las celdas con los valores pasados por los parámetros (código y nombre del producto).
+        Retorna: fila.
+    */
+    modeloCartera(cod, descrip) {
         var fila = document.createElement("tr");
 
         var celdaCod = document.createElement("td");
@@ -45,17 +52,23 @@ class Secc {
         return fila;
     }
 
-    tipoCoste(nombre, coste){
+    /*
+        Método que crea una fila y le añade las celdas con el nombre y el coste de cada costo.
+        Le añade un atributo que cambia el color del borde cuando el ratón pasa por encima y 
+            vuelve al color por defecto cuando se deja de pasar el ratón
+        Retorna: fila .
+    */
+    tipoCoste(nombre, coste) {
         var fila = document.createElement("tr");
 
         var celdaCod = document.createElement("td");
         celdaCod.className = "centra borde";
+
         var celdaDes = document.createElement("td");
         celdaDes.textContent = nombre;
         celdaDes.className = "centra borde";
-        celdaDes.setAttribute("onmouseover", "this.style.border='1px solid blue';");
+        celdaDes.setAttribute("onmouseover", "this.style.border='2px solid blue';");
         celdaDes.setAttribute("onmouseout", "this.style.border='';");
-        
 
         var celdaCos = document.createElement("td");
         celdaCos.textContent = coste;
@@ -68,12 +81,16 @@ class Secc {
         return fila;
     }
 
-    calculoTotal(total){
-         var fila = document.createElement("tr");
+    /*
+        Método que crea una fila y le añade las celdas con Total y el valor del total.
+        Retorna: fila.
+    */
+    calculoTotal(total) {
+        var fila = document.createElement("tr");
 
         var celdaCod = document.createElement("td");
 
-         var celdaDes = document.createElement("td");
+        var celdaDes = document.createElement("td");
         celdaDes.textContent = "TOTAL";
         celdaDes.className = "centra borde";
 
@@ -90,14 +107,20 @@ class Secc {
 }
 
 class Tabla {
+    // Atributo para trabajar con el contenido del JSON parseado
     #contenido = [];
 
-    constructor(){
+  
+    /*
+        Cuando se crea una instancia de esta clase se va a guardar el contenido del JSON parseado en el atributo
+        y se va a ejecutar el método render() automáticamente.
+    */
+    constructor(bdDallo) {
         this.#contenido = JSON.parse(bdDallo);
-        this.render();   
+        this.render();
     }
 
-    render(){
+    render() {
         // Para aplicarlo a partir de la etiqueta body del html
         var contenedor = document.body;
 
@@ -117,28 +140,30 @@ class Tabla {
         cabecera.appendChild(secc.cabecera());
 
         // Recorremos todos los productos del json para usar lo que necesitemos en los métodos de Secc
-        for (let i of this.#contenido){
-            // Montamos el cuerpo de la tabla usando el método con el código y la descripción como parámetros
+        for (let i of this.#contenido) {
+            // Montamos los modelos en el cuerpo de la tabla usando el método con el código y la descripción como parámetros
             bodyTabla.appendChild(secc.modeloCartera(i.cod, i.descrip));
             // Creamos la vv para ir acumulando el total de los costes
             var total = 0;
             // Mientras recorremos los productos volvemos a recorrer el otro array dentro de costos
-            for (let j in i.costos){
+            for (let j in i.costos) {
                 var costo = i.costos[j];                            // El valor de cada tipo de costo
                 total += costo;
-                bodyTabla.appendChild(secc.tipoCoste(j, costo));    // Montamos los costos en la tabla usando nuestro método
+                // Montamos los costos en la tabla usando nuestro método con el nombre y el valor de cada uno como parámetros
+                bodyTabla.appendChild(secc.tipoCoste(j, costo));
             }
-
+            // Montamos el coste total en el cuerpo usando nuestro método con el total como parámetro
             bodyTabla.appendChild(secc.calculoTotal(total.toFixed(2)));
         }
 
+        // Montamos cabecera y cuerpo en la tabla y ésta a su vez en el body del html
         tabla.appendChild(cabecera);
-        tabla.appendChild(bodyTabla);   
+        tabla.appendChild(bodyTabla);
         contenedor.appendChild(tabla);
     }
 }
 
-const bdDallo=`[{ "cod": 1,
+const bdDallo = `[{ "cod": 1,
                         "descrip": "cartera piel vacuno negra",
                         "costos":{ "piel": 5,
                                    "hilo": 0.80,
@@ -175,4 +200,5 @@ const bdDallo=`[{ "cod": 1,
                                    "embasado": 5}               
                    }]`
 
+// Creamos instancia de la clase tabla
 new Tabla(bdDallo);   
