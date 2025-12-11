0753ed57c2e2ee49ce328bcbe7d40e649f66a5ce
Javier RL <colego@hotmail.es>
2025-12-11 13:03:11 +0100

    11/12/25

---
+++ Tema 9 - AJAX/ejemplos/ejemplo3/jsonGET.php
@@ -0,0 +1,11 @@
+<?php
+    // Para solicitudes de otros dominios.
+    header("access-control-allow-origin: *");
+    
+    //......................................
+    $nombre = $_GET['nombre'];
+    $ciudad = $_GET['ciudad'];
+    
+    // Devuelve cadena JSON de objetos
+    echo '{"nombre":"' . $nombre . '","ciudad":"' . $ciudad . '"}';
+?>
