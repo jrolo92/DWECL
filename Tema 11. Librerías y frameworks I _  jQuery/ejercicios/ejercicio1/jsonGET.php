81de7942ef973cf07c19b05fbe93026e577e7f88
Javier RL <colego@hotmail.es>
2026-01-16 13:02:25 +0100

    16/01/26

---
+++ Tema 11. Librerías y frameworks I _  jQuery/ejercicios/ejercicio1/jsonGET.php
@@ -0,0 +1,42 @@
+<?php
+    // Para solicitudes de otros dominios.
+    header("access-control-allow-origin: *");
+
+    // Conectamos BD y configuramos los caracteres
+    $conexion = new mysqli('localhost', 'root', '', 'tema9');
+    $conexion->set_charset("utf8");
+
+    // Obtenemos parámetro id por el método get
+    $nombreID = $_GET['id'] ?? null;
+
+    /*
+        En función de si disponemos del parámetro nos va a devolver una cosa u otra
+    */ 
+    if ($nombreID == "") {
+        // Consulta SQL
+        $sql1 = "SELECT id, nombre FROM datos";
+        // Ejecutamos consulta
+        $stmt = $conexion->query($sql1);
+        // Devolvemos datos en un array asociativo
+        $datos = $stmt->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($datos);
+        // Cerramos conexión
+        $stmt->close();
+
+    } else {
+        // Consulta SQL
+        $sql2 = "SELECT * FROM datos WHERE id = ?";
+        // Hacemos prepare, vinculamos parámetros y ejecutamos la consulta
+        $stmt = $conexion->prepare($sql2);
+        $stmt->bind_param('i', $nombreID);
+        $stmt->execute();
+        // Devolvemos datos de la fila
+        $resultado = $stmt->get_result();
+        $fila = $resultado->fetch_assoc();
+        echo json_encode($fila);
+        // Cerramos conexión
+        $stmt->close();
+    }
+
+    $conexion->close();
+?>
---
+++ Tema 11. Librerías y frameworks I _  jQuery/ejercicios/ejercicio1/modulo1.js
@@ -0,0 +1,13 @@
+
+
+/*
+    Función exportable de acceso a JSON usando Jquery
+    Hace la petición AJAX por el método GET usando la url y una función callback a la que le pasamos el JSON como parámetro
+    pasandolo antes a objeto JSON.
+*/
+export function funAjax(url, fun){
+    $.get(url, function(obj){
+        obj = JSON.parse(obj);
+        fun(obj);
+    });
+}
